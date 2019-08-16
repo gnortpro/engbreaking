bb5397cf1331591fd191cbf93f717313be4252a6
Trong <trongnguyen@x3english.com>
2019-08-16 14:39:10 +0700

    a

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,13 @@ $(function() {
   $(".totop").click(function() {
     $("html, body").animate({ scrollTop: $("#header").offset().top }, 800);
   });
+
+    $(".lazy").recliner({
+        attrib: "data-src", // selector for attribute containing the media src
+        throttle: 300,      // millisecond interval at which to process events
+        threshold: 100,     // scroll distance from element before its loaded
+        live: true          // auto bind lazy loading to ajax loaded elements
+    });
   // effect notification
 
   var namedathangg = [
